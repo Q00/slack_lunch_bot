4fea93a3ada834d4881a29276781a4c1a354faaa
Q00 <jqyu.lee@gmail.com>
2019-01-10 16:27:26 +0900

    (chore) make call class bot

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 from slacker import Slacker
 import json
+import time 
+import websocket
+import bot_class
 
-with open('env.json','r') as f:
-    config=json.load(f)
+def create_bot():
 
-token = config['DEFAULT']['OAUTH_TOKEN'] 
-slack = Slacker(token)
-slack.chat.post_message('#random','test!')
+    with open('env.json','r') as f:
+        config=json.load(f)
+    token = config['DEFAULT']['OAUTH_TOKEN'] 
+    slacker = Slacker(token)
+    #slack.chat.post_message('#random','test!')
+    print(slacker)
+    return bot_class.Bot(slacker)
+
+def run():
+    bot = create_bot()
+    bot.loop()
+if __name__=="__main__":
+    run()
+
