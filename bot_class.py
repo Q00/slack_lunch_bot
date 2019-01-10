43e7f91e95ce0797782eeb6023e5fe635e772898
Q00 <jqyu.lee@gmail.com>
2019-01-10 16:45:20 +0900

    (feat) parsing event and send

--- bot_class.py
+++ bot_class.py
@@ -12,7 +12,9 @@ class Bot:
         print('start loop')
 
         while True:
-            self._read()
+           channel, msg = self._read()
+           print('befor send')
+           self._send(channel,msg)
             
     
     def _read(self):
@@ -28,8 +30,12 @@ class Bot:
                 event = json.loads(self._socket.recv())
                 #{'type': 'message', 'user': 'UEHAZ63AP', 'text': '~점심', 'client_msg_id': '5cce9b39-9d70-4584-9776-9d91c1ab034c', 'team': 'TEH919RMJ', 'channel': 'CEHAZ69A7', 'event_ts': '1547104675.002500', 'ts': '1547104675.002500'}
                 #print(event)
-                
+                print(event) 
                 channel, msg = self._parse(event)
+                if not channel or not  msg:
+                    continue
+                break
+                print('here')
             except websocket.WebSocketTimeoutException:
                 self.ws.send(json.dumbs({'type':'ping'}))
 
@@ -42,14 +48,15 @@ class Bot:
         return channel, msg
 
     def _send(self,channel,msg):
+        msg = msg + '를 선택하셨습니다'
         self.slacker.chat.post_message(channel, msg, as_user=True)
 
     def _parse(self, event):
-        if event['type'] != 'message' or event['text'] == '~점심':
+        if event['type'] != 'message' or event['text'] != '~점심':
             return None,None
 
         channel = event['channel']
-        
+        print(channel) 
         text = event['text'].replace(self.calling_msg, '').strip()
         #전체
         if text == '':
@@ -57,4 +64,5 @@ class Bot:
         #한식, 양식, 중식, 일식 그대로
 
         msg = text
+        print(msg)
         return channel, msg 
