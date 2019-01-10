c612a7b35ab3f0f84d8d22c2399613dce39f9da9
Q00 <jqyu.lee@gmail.com>
2019-01-10 15:06:32 +0900

    (feat) send message

--- bot.py
+++ bot.py
@@ -4,4 +4,6 @@ import json
 with open('env.json','r') as f:
     config=json.load(f)
 
-token = config['DEFAULT']['TOKEN'] 
+token = config['DEFAULT']['OAUTH_TOKEN'] 
+slack = Slacker(token)
+slack.chat.post_message('#random','test!')
